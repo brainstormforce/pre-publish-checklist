0612236916d5a214427d26513accd43518d6d5a3
Nikhil Chavan <email@nikhilchavan.com>
2019-09-17 15:51:21 +0530

    CS fixes

--- classes/class-ppc-pagesetups.php
+++ classes/class-ppc-pagesetups.php
@@ -160,7 +160,7 @@ if ( ! class_exists( 'PPC_Pagesetups' ) ) :
 			wp_enqueue_script( 'ppc_backend_tooltip_js' );
 			wp_enqueue_style( 'ppc_backend_css' );
 			$ppc_checklist_item_data = get_option( 'ppc_checklist_data' );
-			$value                   = get_post_meta( get_the_ID() , '_ppc_meta_key', true );
+			$value                   = get_post_meta( get_the_ID(), '_ppc_meta_key', true );
 			?>
 			<div class="ppc-percentage-wrapper">
 				<span class="ppc-percentage-value"></span>
