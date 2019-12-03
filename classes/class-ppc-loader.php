ed9edd651459c1a4e47d3b710fdbb7cee73998ad
pratiksha <pratikshas@bsf.io>
2019-12-03 16:00:13 +0530

    add_option removed

--- classes/class-ppc-loader.php
+++ classes/class-ppc-loader.php
@@ -84,15 +84,12 @@ if ( ! class_exists( 'PPC_Loader' ) ) :
 				'ppc_key7' => 'Done SEO',
 				'ppc_key8' => 'Spelling and Grammar Checked',
 			);
-
-			add_option( 'ppc_checklist_data', $ppc_default_checklist_data );
-			$ppc_checklist_item_data = get_option( 'ppc_checklist_data', $ppc_default_checklist_data );
+			$ppc_checklist_item_data    = get_option( 'ppc_checklist_data', $ppc_default_checklist_data );
 
 			$default_list = array(
 				'page' => $ppc_checklist_item_data,
 				'post' => $ppc_checklist_item_data,
 			);
-			add_option( 'ppc_cpt_checklist_data', $default_list );
 			return get_option( 'ppc_cpt_checklist_data', $default_list );       // page, post, movie.
 		}
 
