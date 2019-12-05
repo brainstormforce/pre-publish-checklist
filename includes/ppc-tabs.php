efc0dc21a98a5e4e19e6bd529a8d7c27faeb297c
Rahul <Rahullakhwani5175@gmail.com>
2019-12-05 18:44:15 +0530

    Fixed unchecked post checklist

--- includes/ppc-tabs.php
+++ includes/ppc-tabs.php
@@ -45,10 +45,14 @@ if ( isset( $_GET['tab'] ) ) {  //PHPCS:ignore:WordPress.Security.NonceVerificat
 		echo 'nav-tab-active ppc-active-tab';
 	}
 	?>
-	"><?php esc_attr_e( 'General Settings', 'pre-publish-checklist' ); ?></a>
-
-
-		<a href="?page=ppc&tab=ppc-checklist&type=post" class="nav-tab tb 
+	">
+	<?php esc_attr_e( 'General Settings', 'pre-publish-checklist' ); ?></a>
+		<?php
+		$ppc_post_types       = get_option( 'ppc_post_types_to_display' );
+		$ppc_type             = $ppc_post_types[0];
+		$ppc_active_checkpost = ! empty( $ppc_post_types ) ? $ppc_type : 'post';
+		?>
+		<a href="?page=ppc&tab=ppc-checklist&type=<?php echo esc_attr( $ppc_active_checkpost ); ?>" class="nav-tab tb 
 	<?php
 	if ( 'ppc-checklist' === $ppc_active_tab ) {
 		echo 'nav-tab-active ppc-active-tab';
